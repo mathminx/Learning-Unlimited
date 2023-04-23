5ae47cb22801c6a5cef9264bf7750070cac5d631
MSchuur <mschuur31@gmail.com>
2023-04-23 11:45:34 -0300

    Poputaled the model index.js file with the relationships required. Did not create the student and tutor to subjet relationship as I need to discuss the best way to implement this relationship with the team.

--- model/index.js
+++ model/index.js
@@ -4,11 +4,40 @@ const Tutor = require('./TutorTutor');
 const Student = require('./Student');
 const Subject = require('./Subject');
 
+User.hasOne(Address, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
+Address.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
+User.hasOne(Tutor, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
+Tutor.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
+Tutor.hasMany(Student, {
+    foreignKey: 'tutor_id',
+    OnDelete: 'CASCADE',
+});
 
+Student.belongsTo(Tutor, {
+    foreignKey: 'tutor_id',
+});
 
+Student.hasMany(Tutor, {
+    foreignKey: 'student_id',
+    OnDelete: 'CASCADE'
+});
+
+Tutor.belongsTo(Student, {
+    foreignKey: 'student_id',
+});
 
 module.exports = { User, Address, Tutor, Student, Subject };
