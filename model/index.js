b98bdd48599da7fb265c4763be6286b5ab290cf5
MSchuur <mschuur31@gmail.com>
2023-04-23 11:16:57 -0300

    Created and populated the subjects model and updated the index.js file in the models folder.

--- model/index.js
+++ model/index.js
@@ -2,6 +2,7 @@ const User = require('./User');
 const Address = require('./Address');
 const Tutor = require('./TutorTutor');
 const Student = require('./Student');
+const Subject = require('./Subject');
 
 
 
@@ -10,4 +11,4 @@ const Student = require('./Student');
 
 
 
-module.exports = { User, Address, Tutor, Student  }
+module.exports = { User, Address, Tutor, Student, Subject };
