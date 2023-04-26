6d72dc7c4c5fbe577f86a1e502a7f2854815da80
“anar <sehar.uzair23@gmail.com>
2023-04-26 18:53:54 -0300

    index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const tutorRoutes = require('./tutorRoutes');
+const studentRoutes = require('./studentRoutes');
+const subjectRoutes = require('./subjectRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/Tutor', tutorRoutes );
+router.use('/Student', studentRoutes);
+router.use('/Subject', subjectRoutes );
+router.use('/User', userRoutes);
+
+module.exports = router;
---
+++ controllers/api/tutorRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Tutor } = require('../../model');
+const withAuth = require('../../utils/auth');
+
+//   router.post('/', async (req, res) => {
+//     try {
+//       const tutorData = await Tutor.create(req.body);
+  
+//       req.session.save(() => {
+//         req.session.tutor_id = tutorData.id;
+//         req.session.logged_in = true;
+  
+//         res.status(200).json(tutorData);
+//       });
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
+
+
+
+
+
+module.exports = router;
