6374c65874b7d71dc58344aed35ff54205796da1
“anar <sehar.uzair23@gmail.com>
2023-04-26 21:00:54 -0300

    tutor routes created

--- controllers/api/tutorRoutes.js
+++ controllers/api/tutorRoutes.js
@@ -3,39 +3,60 @@ const { Tutor } = require('../../model');
 const withAuth = require('../../utils/auth');
 
 //router.get
-
-
-//router.get(by id)
-
-//   router.post('/', async (req, res) => {
-//     try {
-//       const tutorData = await Tutor.create(req.body);
+router.get('/', async (req, res) => {
+    res.render('all', {tutor});
+  });
   
-//       req.session.save(() => {
-//         req.session.tutor_id = tutorData.id;
-//         req.session.logged_in = true;
-  
-//         res.status(200).json(tutorData);
-//       });
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
+
+//   router.get('/tutor/:num', async (req, res) => {
+//     return res.render('tutor', tutors[req.params.num - 1]);
 //   });
+  
 
-//router.put('/',async (req, res) => {
-//     try {
-//       const tutorData = await Tutor.create(req.body);
+
+router.get('/tutor/:id', async (req, res) => {
+    try {
+      const tutorData = await Tutor.findByPk(req.params.id);
+      console.log(TutorData)
+      const tutor = dblearningUnlimited.get({ plain: true });
+
+      res.render('tutor', { tutor });
+      res.render('tutor', tutorData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+  router.post('/', async (req, res) => {
+    try {
+      const tutorData = await Tutor.create(req.body);
   
-//       req.session.save(() => {
-//         req.session.tutor_id = tutorData.id;
-//         req.session.logged_in = true;
+      req.session.save(() => {
+        req.session.tutor_id = tutorData.id;
+        req.session.logged_in = true;
   
-//         res.status(200).json(tutorData);
-//       });
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
+        res.status(200).json(tutorData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+router.put('/',async (req, res) => {
+    try {
+      const tutorData = await Tutor.create(req.body);
+  
+      req.session.save(() => {
+        req.session.tutor_id = tutorData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(tutorData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 
 
