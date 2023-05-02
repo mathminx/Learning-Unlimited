c6fd081cd23a107d704b7e9757dbd841e0996aae
“anar <sehar.uzair23@gmail.com>
2023-05-02 17:35:49 -0300

    triny to fix route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,136 +1,114 @@
 const router = require('express').Router();
-const { User } = require('../../model');
+const { User, Tutor, Student, Address, Subject } = require('../../model');
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
-  // Send the rendered login page back as the response
-  res.render('homepage');
-});
-
-router.get('/login', async (req, res) => {
-  // Send the rendered login page back as the response
-  res.render('login');
-});
-
-router.post('/', async (req, res) => {
   try {
-    // Find the user who matches the posted e-mail address
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
+    const userData = await User.findAll({
+      include:[Student, Tutor, Address]});
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });const router = require('express').Router();
-        const {User} = require("../model");
-        
-        router.get('/', async (req, res) => {
-          // Send the rendered Handlebars.js template back as the response
-          res.render('login');
-        });
-        
-        module.exports = router;
-        
+      res.status(400).json({ message: "Unable to retrieve tutor data."});
       return;
     }
-
-    // Verify the posted password with the password store in the database
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    // Create session variables based on the logged in user
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
+    res.status(200).json(userData);
+  }
+  catch(err) {
+    res.status(500).json(err);
   }
 });
-
-
-router.put('/:id', (req, res) => {
-  User.update(req.body,{where:{user_id:req.params.id}})
-  .then(userData => res.status(200).json(userData))
-  .catch (err => res.status(400).json(err));
-});
-
-
-
-  router.delete('/:id', withAuth, async (req, res) => {
-        try {
-          const userData = await User.destroy({
-            where: {
-              id: req.params.id,
-              user_id: req.session.user_id,
-            },
-          });
-      
-          if (!userData) {
-            res.status(404).json({ message: 'user ID incorrect.' });
-            return;
-          }
-      
-          res.status(200).json(userData);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      });
-    
-
-router.put('/:id', async (req, res)=> {
-  try{
-    const UserData =  await User.create(req.body);
-
-    req.session.save(() => {
-      req.sessionID.User_id = UserData.id;
-      req.session.logged_in = true;
-      res.status(200).json(UserData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});     
-
-router.post('/:id', async (req, res)=> {
-  try{
-    const UserData = await User.create(req.body);
-
-
-    req.session.save(() => {
-      req.session.User_id = userDAta.id;
-      req.session.logged_in = true;
-      res.status(200).json(UserData);
-    });
-  } catch (err) {res.status(400).json(err)};
-});
-
-router.delete('/:id', withAuth, async (req, res) => {
+//router.get('/:id', async (req, res) => {
+  //try {
+    //const userData = await User.findOne({where:{user_id:req.params.id}, include:[Student, Tutor, Address]});
+    //res.status(200).json(userData);
+  //}
+  //catch(err) {
+    //res.status(500).json(err);
+  //}
+//});
+//router.post('/', async (req, res) => {
+    //try {
+      //const UserData = await User.create(req.body);
+      //req.session.save(() => {
+       // req.session.User_id = UserData.id;
+        //req.session.logged_in = true;
+       // res.status(200).json(userData);
+      //});
+    //} catch (err) {
+     // res.status(400).json(err);
+   // }
+  //});
+//router.put('/:id', (req, res) => {
+  //User.update(req.body,{where:{user_id:req.params.id}})
+  //.then(userData => res.status(200).json(userData))
+  //.catch (err => res.status(400).json(err));
+//});
+  //router.delete('/:id', withAuth, async (req, res) => {
+        //try {
+          //const userData = await User.destroy({
+            //where: {
+              //id: req.params.id,
+              //user_id: req.session.user_id,
+            //},
+          //});
+          //if (!userData) {
+            //res.status(404).json({ message: 'user ID incorrect.' });
+            //return;
+         // }
+         //res.status(200).json(userData);
+       // } catch (err) {
+         // res.status(500).json(err);
+       // }
+      //});
+router.get('/:id', async (req, res) => {
   try {
-    const UserData = await UserData.destroy({
-      id: req.params.id,
-      user_id: req.session.user_id,
-    });
-
-
-    if (UserData) {
-      res.status(404).json('no user found.');
-    }
-    res.status(200).json(UserData);
+    const userData = await Subject.findByPk(req.params.id, {include: [Student, tutor, Subject] });
+    console.log(userData)
+    const user = userData.get({ plain: true });
+    res.status(200).json(user);
+    //res.render('user', { user });
   } catch (err) {
     res.status(500).json(err);
   }
-  
 });
-
-
-
-
-
+router.post('/', async (req, res)=> {
+  try{
+    const UserData = await User.create(req.body);
+    //req.session.save(() => {
+      //req.session.User_id = userDAta.id;
+      //req.session.logged_in = true;
+    //});
+    res.status(200).json(UserData);
+  } catch (err) {res.status(400).json(err);}
+});
+router.put('/:id', async (req, res) => {
+  try {
+    console.log(req.params.id, req.body)
+    const userData = await User.update(req.body, {where:{user_id:req.params.id}});
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;
+    //  });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err)
+  };
+});
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const userData = await User.destroy({where:{user_id:req.params.id}})
+    // user.destroy({
+    //   where: {
+    //     id: req.params.id,
+    //     user_id: req.session.user_id,
+    //   },
+    // });
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id. please try again.' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err)
+  };
+});
+module.exports =  router;
